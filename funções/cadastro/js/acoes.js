69591a076df48690bfb602e80a7d1505f175b6d8
HugoEngDev <hugaogola@gmail.com]>
2023-08-07 17:23:12 -0300

    inicio do Js

---
+++ funções/cadastro/js/acoes.js
@@ -0,0 +1,14 @@
+let nome = document.getElementById('nome')
+let idade = document.getElementById('idade')
+let salario = document.getElementById('salario')
+let cargo = document.getElementById('cargo')
+alert('let go')
+
+function salvar(){
+    event.preventDefault()
+    document.getElementById('fnome').innerHTML = nome.value
+    document.getElementById('fidade').innerHTML = nome.value
+    document.getElementById('fsalario').innerHTML = nome.value
+    document.getElementById('fnome').innerHTML = nome.value
+}
+
